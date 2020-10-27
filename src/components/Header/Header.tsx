c583a68eb39e2dba2ac2a631f1a465b6aa141eda
Thiago Maceda <thiagopmaceda@gmail.com>
2020-10-27 13:46:43 -0300

    feat: add Header and components folder

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+    return (
+        <div>
+            <header>
+                <h2>
+                    Where in the world?
+        </h2>
+            </header>
+
+        </div>
+    );
+}
+
+export default Header;
